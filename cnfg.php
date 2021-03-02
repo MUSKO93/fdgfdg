2383b8d2bd2273af6848c1a3999c7368a5bfaa8b
MUSKO93 <64425556+MUSKO93@users.noreply.github.com>
2021-03-02 22:33:18 +0100

    Add files via upload

---
+++ cnfg.php
@@ -0,0 +1,12 @@
+<?php
+
+/*Configs By HACK-TO-WIN*/
+
+
+// Add your email //
+$info = 'https://megacash.me/server/backend/credits/setClientRewarded.php?email=musko2929@outlook.com&removeCredits=0&which=GPlay';
+
+?>
+
+
+
